468d74d65a5c9534eb88e11b333c258f13bf3d5c
Niradity Emmanuel <yadavrjking66997@gmail.com>
2023-11-05 01:57:55 +0530

    Dyanamic nav bars both device mobile and desktop view design

---
+++ portfolioapp/src/routes/Routings.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbars from '../modules/Navbars'
+
+const Routings = () => {
+  return (
+    <div>
+      <Navbars />
+    </div>
+  )
+}
+
+export default Routings
